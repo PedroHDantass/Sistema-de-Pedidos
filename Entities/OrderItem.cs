e071dbf6c38591dee95c245533efface22c051d4
PedroHDantas <pedrohdantaspc@gmail.com>
2025-07-17 17:44:29 -0300

    feat: implementar Class OrderItem, suas propriedades e métodos

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.Entities
+{
+    internal class OrderItem
+    {
+        public int Quatity { get; set; }
+
+        public double Price { get; set; }
+
+        public double SubTotal()
+        {
+            return Price * Quatity;
+        }
+    }
+}
